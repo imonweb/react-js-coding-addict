274137d8ac50ced96c358dc1c733f6331c5905a2
Imon <contact@imonweb.co.uk>
2024-01-19 01:36:37 +0000

    forms: controlled inputs

--- advanced-react/src/App.jsx
+++ advanced-react/src/App.jsx
@@ -9,7 +9,8 @@ import { data } from './data';
 // import Navbar from './tutorial/04-project-structure/starter/Navbar'
 // import { Home, About } from './tutorial/04-project-structure/starter/Pages'
 // import Example from './tutorial/04-project-structure/starter/Example'
-import List from './tutorial/05-leverage-javascript/starter/List';
+// import List from './tutorial/05-leverage-javascript/starter/List';
+import Form from './tutorial/06-forms/starter/01-controlled-inputs';
  
 
 
@@ -23,7 +24,7 @@ function App() {
   return (
     <>
       <div className='container'>
-        <List />
+        <Form />
       
       </div>
       
