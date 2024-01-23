d6a3952ba9f8e95add01b890efea024ebd858c70
Imon <contact@imonweb.co.uk>
2024-01-23 09:53:00 +0000

    global context api

--- global-context/src/main.jsx
+++ global-context/src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import AppContext from './Context.jsx'
-// import Context from './Context.jsx'
- 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
