3982aa4a0e6d41c262bc5f9bec4ee12b41549f95
Imon <contact@imonweb.co.uk>
2024-01-27 23:07:22 +0000

    birthday-buddy

--- fundamental-projects/01-birthday-buddy/src/App.jsx
+++ fundamental-projects/01-birthday-buddy/src/App.jsx
@@ -4,19 +4,45 @@ import data from './data'
 const App = () => {
   const [people, setPeople] = useState(data)
 
+  const clearAllPeople = (e) => {
+    e.preventDefault()
+    setPeople([])
+  }
+
+  // let count = 0;
+  // for(let i = 0; i < people.length; i++) {
+  //   if(people[i].status === '0') count++;
+  // }
+
+  // console.log(count)
+
+  let total = data.length;
+  console.log(total)
+
   return (
   <>
-    <h1>Birthdays Today</h1>
+      <form className='form'>
+
+        <h1>{total} Birthdays Today</h1>
+        <br />
     {people.map((person) => {
       console.log('person')
       return (
-        <div>
+        <>
             <ul>
-              <li>{person.id} {person.name} {person.age}</li>
+              <li className='person'>
+                <img className="img" src={person.image}  alt="" />
+                <div className="main">
+                  <h4>{person.name} </h4> 
+                  <p>{person.age} years </p>
+                </div>
+              </li>
             </ul>
-        </div>
+        </>
       )
     })}
+        <button className='btn btn-block' onClick={clearAllPeople}>Clear All</button>
+    </form>
   </>
   )
 };
