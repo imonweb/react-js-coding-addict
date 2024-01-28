7ec258f60a0e9eab357d39095e3eaef88565a9a8
Imon <contact@imonweb.co.uk>
2024-01-28 23:54:13 +0000

    birthday-buddy

--- fundamental-projects/01-birthday-buddy/src/App.jsx
+++ fundamental-projects/01-birthday-buddy/src/App.jsx
@@ -1,49 +1,24 @@
 import { useState } from 'react';
 import data from './data'
+import List from './List';
   
 const App = () => {
   const [people, setPeople] = useState(data)
 
-  const clearAllPeople = (e) => {
-    e.preventDefault()
-    setPeople([])
-  }
-
-  // let count = 0;
-  // for(let i = 0; i < people.length; i++) {
-  //   if(people[i].status === '0') count++;
-  // }
-
-  // console.log(count)
-
-  let total = data.length;
-  console.log(total)
-
   return (
-  <>
-      <form className='form'>
-
-        <h1>{total} Birthdays Today</h1>
-        <br />
-    {people.map((person) => {
-      console.log('person')
-      return (
-        <>
-            <ul>
-              <li className='person'>
-                <img className="img" src={person.image}  alt="" />
-                <div className="main">
-                  <h4>{person.name} </h4> 
-                  <p>{person.age} years </p>
-                </div>
-              </li>
-            </ul>
-        </>
-      )
-    })}
-        <button className='btn btn-block' onClick={clearAllPeople}>Clear All</button>
-    </form>
-  </>
+    <main>
+      <section className="container">
+        <h3>{people.length} Birthday's Today</h3>
+        <List people={people} />
+        <button
+          type='button'
+          className='btn btn-block'
+          onClick={() => setPeople([])}
+        >
+          Clear Items
+        </button>
+      </section>
+    </main>
   )
-};
+}
 export default App;
---
+++ fundamental-projects/01-birthday-buddy/src/List.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Person from './Person'
+
+const List = ({people}) => {
+  
+  return (
+    <section>
+     {people.map((person) => {
+      console.log(person)
+      return <Person key={person.id} {...person} />
+     } )}
+    </section>
+  )
+}
+
+export default List
