c22aafc302310c3fe9cfac9c3395d579de3e1caf
Imon <contact@imonweb.co.uk>
2024-01-29 22:13:51 +0000

    tours

--- fundamental-projects/02-tours/src/App.jsx
+++ fundamental-projects/02-tours/src/App.jsx
@@ -1,6 +1,64 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const url = 'https://course-api.com/react-tours-project';
+ 
 
 const App = () => {
-  return <h2>Tours Starter</h2>;
+  const [tours, setTours] = useState([]);
+  const [isError, setIsError] = useState(false);
+
+  
+
+  useEffect( () => {
+    const fetchTours = async () => {
+      try {
+        const response = await fetch(url);
+        const tours = await response.json();
+        
+        setTours(tours);
+      } catch(error){
+        console.log(error);
+      }
+    }
+    fetchTours();
+  }, [])
+
+  // if(isLoading) {
+  //   return <h2>Loadding...</h2>
+  // }
+
+  // if(isError) {
+  //   return <h2>There was an error!</h2>
+  // }
+
+  const removeTour = (id) => {
+    setTours(tours.filter((tour) => tour.id !== id))
+  }
+
+
+  return (
+    <main>
+      <h1>Our Tours</h1>
+      <ul className="tours">
+      {tours.map((tour) => {
+        return (
+            <li className="single-tour">
+              <span>
+                <div className="tour-price">{tour.price}</div>
+                <img src={tour.image} className="img" alt="" />
+              </span>
+              <h5>{tour.name}</h5>
+              <div className="tour-info">
+                {tour.info}
+              </div>
+              <button className="btn" onClick={() => removeTour(id)}>Not Interested</button>
+
+            </li>
+        )
+      })}
+      </ul>
+    </main>
+  )
 };
 export default App;
---
+++ fundamental-projects/02-tours/src/Tours.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Tours = () => {
+  return (
+    <main>
+      <div>Tours</div>
+      <div className="tours">
+        {}
+      </div>
+    </main>
+  )
+}
+
+export default Tours
