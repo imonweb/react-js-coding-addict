3f6575de6a0eee43de4b17299b67c4d7d6c03f94
Imon <contact@imonweb.co.uk>
2024-02-02 12:56:35 +0000

    lorem-ipsum generator

--- fundamental-projects/08-lorem-ipsum/src/App.jsx
+++ fundamental-projects/08-lorem-ipsum/src/App.jsx
@@ -1,4 +1,53 @@
+import { useState } from "react";
+import data from './data'
+import { nanoid } from 'nanoid';
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(1)
+  const [text, setText] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // console.log(typeof count)
+    // console.log(1)
+    let amount = parseInt(count)
+    setText(data.slice(0, amount))
+  }
+
+  return ( 
+    <section className="section-center">
+      <h4>Tired of boring lorem ipsum?</h4>
+      <form 
+        className="lorem-form"
+        onSubmit={handleSubmit}
+      >
+        <label htmlFor="amount">paragraph:</label>
+        <input 
+          type="number" 
+          name="amount" 
+          id="amount" 
+          min="1" 
+          step="1" 
+          max="8" 
+          value={count} 
+          onChange={ (e) => {
+            setCount(e.target.value);
+          }}
+        />
+        <button
+          className="btn"
+          type="submit"
+        >
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          // return <p key={index}>{item}</p>
+          return <p key={nanoid()}>{item}</p>;
+        })}
+      </article>
+    </section>
+  )
 };
 export default App;
