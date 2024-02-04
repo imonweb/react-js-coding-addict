9baf554bddb6cd2be7edc48e912176ce0c37ab3b
Imon <contact@imonweb.co.uk>
2024-02-04 22:36:07 +0000

    grocery bud

--- fundamental-projects/10-grocery-bud/src/App.jsx
+++ fundamental-projects/10-grocery-bud/src/App.jsx
@@ -3,23 +3,59 @@ import Form from "./Form";
 import {nanoid} from 'nanoid'
 import Items from "./Items";
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+  // console.log(list);
+  if(list){
+    list = JSON.parse(localStorage.getItem('list'));
+  } else {
+    list = []
+  }
+  return list
+}
+
+const setLocalStorage = (items) => {
+  localStorage.setItem('list', JSON.stringify(items))
+}
+
+const defaultList = JSON.parse(localStorage.getItem('list') || '[]')
+
 const App = () => {
-  const [items, setItems] = useState([])
+  // getLocalStorage();
+  const [items, setItems] = useState(getLocalStorage());
   const addItem = (itemName) => {
     const newItem = {
       name: itemName,
       completed: false,
       id: nanoid(),
     }
-    setItems([...items, newItem])
+    const newItems = [...items, newItem]
+    setItems(newItems)
+    setLocalStorage(newItems)
   }
 
-  const removeItem = (itemId) => {}
+  const removeItem = (itemId) => {
+    const newItems = items.filter((item) => item.id !== itemId)
+    setItems(newItems)
+    setLocalStorage(newItems)
+  }
+
+  const editItem = (itemId) => {
+    const newItems = items.map( (item) => {
+      if(item.id !== itemId){
+        const newItem = {...item, completed:!item.completed}
+        return newItem
+      }
+      return item
+    } )
+    setItems(newItems)
+    setLocalStorage(newItems)
+  }
 
   return ( 
     <section className="section-center">
       <Form addItem={addItem} />
-      <Items items={items} removeItem={removeItem} />
+      <Items items={items} removeItem={removeItem}  editItem={editItem}/>
     </section>
   )
 };
