c37d049b1bc201eb4b07904d0b1a13c8ee7b4d4a
Imon <contact@imonweb.co.uk>
2024-02-08 23:58:52 +0000

    sidebar

---
+++ fundamental-projects/12-sidebar/src/context.jsx
@@ -0,0 +1,40 @@
+import { createContext, useState, useContext } from "react";
+
+const AppContext = createContext();
+
+export const AppProvider = ({children})  => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+  const [isModelOpen, setIsModelOpen] = useState(false)
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true)
+  }
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false)
+  }
+
+  const openModal = () => {
+    setIsModelOpen(true)
+  }
+
+  const closeModal = () => {
+    setIsModelOpen(false)
+  }
+
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isModalOpen,
+        openModal,
+        openSidebar,
+        closeModal,
+        closeSidebar
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
+}
