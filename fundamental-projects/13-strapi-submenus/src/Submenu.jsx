3736222b8cd5482fffe987e426f905173cf0ec7d
Imon <contact@imonweb.co.uk>
2024-02-14 23:59:32 +0000

    strapi finsished

--- fundamental-projects/13-strapi-submenus/src/Submenu.jsx
+++ fundamental-projects/13-strapi-submenus/src/Submenu.jsx
@@ -1,14 +1,44 @@
 import React from 'react'
 import sublinks from './data'
 import { useGlobalContext } from './Context'
+import { useRef } from 'react'
 
 const Submenu = () => {
-  const {pageId} = useGlobalContext()
+  const {pageId, setPageId} = useGlobalContext()
   const currentPage = sublinks.find((item) => item.pageId === pageId)
-  console.log(currentPage);
+  // console.log(currentPage);
+
+  const submenuContainer = useRef(null)
+
+  const handleMouseLeave = (event) => {
+    const submenu = submenuContainer.current;
+    const { left, right, bottom } = submenu.getBoundingClientRect();
+    const { clientX, clientY } = event;
+
+    if (clientX < left + 1 || clientX > right - 1 || clientY > bottom - 1) {
+      setPageId(null);
+    }
+  };
+
   return (
-    <div className='submenu'>
+    <div
+      className={currentPage ? 'submenu show-submenu' : 'submenu'}
+      onMouseLeave={handleMouseLeave}  
+    >
       <h5>{currentPage?.page}</h5>
+      <div 
+        className='submenu-links'
+        style={{gridTempleColumns:currentPage?.links?.length > 3 ? '1fr 1fr' : '1fr'}}
+        >
+        {currentPage?.links?.map((link) => {
+          const {id, url, label, icon} = link 
+          return (
+            <a href={url} key={id}>
+              {icon} {label}
+            </a>
+          )
+        })}
+      </div>
     </div>
   )
 }
