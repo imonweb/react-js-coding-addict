43856115b2889c8b2d241929a267d64336638c11
Imon <contact@imonweb.co.uk>
2024-02-16 23:42:07 +0000

    axios custom instance

--- axios/src/App.js
+++ axios/src/App.js
@@ -1,9 +1,16 @@
 import Title from './components/Title';
-import Setup
+// import Setup from './examples/1-first-request'
+// import Setup from './examples/2-headers'
+// import Setup from './examples/3-post-request'
+// import Setup from './examples/4-global-instance'
+import './axios/global'
+import Setup from './examples/5-custom-instance'
+
 function App() {
   return (
     <main>
       <Title />
+      <Setup />
     </main>
   );
 }
