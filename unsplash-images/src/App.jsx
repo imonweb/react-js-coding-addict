038df1810a6b129051feb204de61cd1fb6f20d9d
Imon <contact@imonweb.co.uk>
2024-02-24 23:04:08 +0000

    unsplash images

--- unsplash-images/src/App.jsx
+++ unsplash-images/src/App.jsx
@@ -1,4 +1,16 @@
+// import { useState } from "react";
+import Gallery from "./Gallery";
+import SearchForm from "./SearchForm";
+import ThemeToggle from "./ThemeToggle";
+
+  
 const App = () => {
-  return <h1>Unsplash Images Starter</h1>;
+  return (
+    <main>
+      <ThemeToggle />
+      <SearchForm />
+      <Gallery />
+    </main>
+  )
 };
 export default App;
