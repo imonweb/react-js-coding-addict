e9f82bdcf141272066aae3d1002ce4116f8fc8ab
Imon <contact@imonweb.co.uk>
2024-02-26 22:43:35 +0000

    unsplash dev api

--- unsplash-images/src/SearchForm.jsx
+++ unsplash-images/src/SearchForm.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
 
 const SearchForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // console.log(e.target.elements);
+    const searchValue = e.target.elements.search.value 
+    if(!searchValue) return
+    console.log(searchValue);
+  }
   return (
-    <div>SearchForm</div>
+    <section>
+      <h1 className='title'>unsplash images</h1>
+      <form 
+        onSubmit={handleSubmit}
+        className="search-form"
+      >
+        <input 
+          type="text"
+          className='form-input search-input' 
+          name='search'
+          placeholder='cat'
+        />
+        <button
+          type='submit'
+          className='btn'
+        >
+          search
+        </button>
+      </form>
+    </section>
   )
 }
 
