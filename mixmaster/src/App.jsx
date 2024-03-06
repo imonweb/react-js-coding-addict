040ad3ed2f1faba6adfd1f80d2c9f85be21d37d1
Imon <contact@imonweb.co.uk>
2024-03-06 09:31:20 +0000

    cocktail

--- mixmaster/src/App.jsx
+++ mixmaster/src/App.jsx
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
         loader: landingLoader,
       },
       {
-        path: 'cocktail',
+        path: 'cocktail/:id',
         element: <Cocktail />
       },
       {
