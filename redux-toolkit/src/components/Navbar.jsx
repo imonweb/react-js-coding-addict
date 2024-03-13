caa55d695d4ca53a8a3ff99e9064bcd70634625f
Imon <contact@imonweb.co.uk>
2024-03-13 23:45:41 +0000

    redux-toolkit features

---
+++ redux-toolkit/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import {CartIcon} from '../icons'
+import { useSelector } from 'react-redux'
+
+const Navbar = () => {
+  return (
+    <>
+    <nav>
+      <div className="nav-center">
+        <h3>redux toolkit navbar</h3>
+      </div>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
